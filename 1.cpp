d5c4c41b16c5433aac3f384127a579f4a1508d9c
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2023-10-06 23:46:15 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 
-int main() {
-    cout <<"Hello, World!";
+int main(){
+    // arrays cna be used to store multiple values ina single data type.
+
+    int arr[5]; // decleration of array.
+
+    // in array indexing starts from 0;
+
+    // print five int in reverse order.
+
+    int a,b,c,d,e;
+
+    cout << "Give five integers values : ";
+    cin >>a>>b>>c>>d>>e;
+
+    cout << "the integers in reverse order are: ";
+    cout << e<<d<<c<<b<<a;
+
     return 0;
 }
